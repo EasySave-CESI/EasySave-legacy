a43b4b15f46d9c11dcd7bc19fd925d6a6582e6f9
Imad-54 <imad.elmassi@viacesi.fr>
2023-12-04 09:41:55 +0100

    MenuView creation

---
+++ easysave/src/views/MenuView.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EasySaveApp.controller;
+using EasySaveApp.model;
+
+namespace easysave
+{
+    class MenuView
+    {
+        public void DisplayMenu()
+        { 
+
+        }
+    }
+}
